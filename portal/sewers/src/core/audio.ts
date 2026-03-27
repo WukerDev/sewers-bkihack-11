971b8cca75d88424d46a9363b90007fd53173b21
WukerDev <kozakowski.wiktor@gmail.com>
2026-03-27 23:58:39 +0100

    Dodanie warninga

--- portal/sewers/src/core/audio.ts
+++ portal/sewers/src/core/audio.ts
@@ -2,7 +2,8 @@ export function playSystemSound(type: 'success' | 'error' | 'info') {
   const sounds = {
     success: '/sounds/success.wav',
     error: '/sounds/error.wav',
-    info: '/sounds/info.wav'
+    info: '/sounds/info.wav',
+      warning: '/sounds/warning.wav'
   }
   const audio = new Audio(sounds[type])
   audio.play().catch(() => {})
