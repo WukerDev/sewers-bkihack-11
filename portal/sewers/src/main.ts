e7e8d93e9083832db874b1e8aa3ac5adc4aaba32
WukerDev <kozakowski.wiktor@gmail.com>
2026-03-27 22:50:54 +0100

    Poprawki

--- portal/sewers/src/main.ts
+++ portal/sewers/src/main.ts
@@ -3,6 +3,7 @@ import { registerPlugins } from '@/plugins'
 import App from './App.vue'
 import 'unfonts.css'
 import { createPinia } from 'pinia'
+import './main.css'
 
 const app = createApp(App)
 
--- portal/sewers/src/plugins/vuetify.ts
+++ portal/sewers/src/plugins/vuetify.ts
@@ -11,9 +11,38 @@ import '@mdi/font/css/materialdesignicons.css'
 
 import 'vuetify/styles'
 
-// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
-  theme: {
-    defaultTheme: 'system',
-  },
+theme: {
+  defaultTheme: 'light',
+  themes: {
+    light: {
+      dark: false,
+      colors: {
+        primary: '#00A3E8',
+        secondary: '#8CC63F',
+        background: '#E6F2FF',
+        surface: '#FFFFFF',
+        accent: '#00D1FF',
+        error: '#FF3366',
+        info: '#00C0FF',
+        success: '#32CD32',
+        warning: '#FFB900'
+      }
+    },
+    dark: {
+      dark: true,
+      colors: {
+        primary: '#00BFFF',
+        secondary: '#00FF7F',
+        background: '#051024',
+        surface: '#1A2B4C',
+        accent: '#00FFFF',
+        error: '#FF4D4D',
+        info: '#4AC8FA',
+        success: '#4CAF50',
+        warning: '#FFD700'
+      }
+    }
+  }
+}
 })
