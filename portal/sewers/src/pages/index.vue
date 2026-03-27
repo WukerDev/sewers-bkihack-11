e24f73fdd4be802cc2323981a6d2b97dce814a80
WukerDev <kozakowski.wiktor@gmail.com>
2026-03-27 19:56:34 +0100

    Innit

---
+++ portal/sewers/src/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-card-title>Sewer</v-card-title>
+          <v-card-text>Witaj na stronie startowej!</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+</script>
