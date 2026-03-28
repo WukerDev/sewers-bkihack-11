415293bd13da4f4ceecaf6de403172976eea081f
WukerDev <kozakowski.wiktor@gmail.com>
2026-03-28 02:06:29 +0100

    Workflow

--- portal/sewers/src/main.ts
+++ portal/sewers/src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import { registerPlugins } from '@/plugins'
+import { registerPlugins } from './plugins'
 import App from './App.vue'
 import 'unfonts.css'
 import { createPinia } from 'pinia'
